268daf1b24f6fd6492845dd8cd3c683b0a1e705e
William Andersson <anderssonwilliam2003@gmail.com>
2024-03-01 11:31:27 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="logodiv">
+      <img alt="YatzyWeb logo" class="logo" src="@/assets/logo.svg" width="80" height="80" />
+      <h1>YatzyWeb</h1>
+    </div>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Hem</RouterLink>
+        <RouterLink to="/about">Om projektet</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="routerviewbody">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+@media (max-width: 1023px) {
+  .routerviewbody {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+}
+@media (min-width: 1024px) {
+  .routerviewbody {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+.logodiv {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.logodiv h1 {
+  color: white;
+}
+
+nav {
+  width: 100%;
+  font-size: 18px;
+  text-align: center;
+  margin-top: 1.2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header">
+    <h1>Välkommen till YatzyWeb</h1>
+    <h3>Ett projekt av William Andersson</h3>
+    <div class="btndiv">
+      <button class="btn">LOGGA IN</button>
+      <button class="btn">SKAPA KONTO</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/main.css';
+@media (max-width: 1023px) {
+  .header {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 0.5rem;
+  }
+  .btndiv {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    padding-top: 1rem;
+  }
+}
+@media (min-width: 1024px) {
+  .header {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  .btndiv {
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    padding-top: 1.5rem;
+  }
+}
+
+.btn {
+    padding: 1rem;
+    margin: 0.5rem;
+    background: hsla(160, 100%, 37%, 1);
+    color: white;
+    border-radius: 0.5rem;
+    border: none;
+  }
+.btn:hover {
+  background: rgb(1, 134, 90);
+  color: black;
+}
+.btn:active {
+  background: rgb(1, 70, 47);
+}
+</style>
