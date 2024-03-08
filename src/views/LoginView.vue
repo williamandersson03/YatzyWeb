4e4061233825e96923e385350b1223c294566548
William Andersson <anderssonwilliam2003@gmail.com>
2024-03-08 11:55:36 +0100

    .

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,29 @@
 <template>
-    <h1>{{ answer.answer }}</h1>
-    <img :src="answer.image" alt="answer" />
-</template>
+    <div class="header">
+      <h1>Logga In</h1>
+      <h3>Under uppbyggnad</h3>
+    </div>
+  </template>
   
-  <script lang="ts">
-  import axios from "axios";
-  import { defineComponent } from 'vue'
-
-  interface Answer {
-  answer: string;
-  image: string;
-}
+  <style scoped>
+  @import '../assets/main.css';
+  @media (max-width: 1023px) {
+    .header {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 0.5rem;
+    }
+  }
+  @media (min-width: 1024px) {
+    .header {
+      min-height: 100vh;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+  </style>
   
-  export default  defineComponent({
-    name: "App",
-    data() {
-      return {
-        answer: {} as Answer,
-      };
-    },
-    methods: {
-      async getAnswer() {
-        const { data } = await axios.get("https://yesno.wtf/api");
-        this.answer = data;
-      },
-    },
-    beforeMount() {
-      this.getAnswer();
-    },
-  });
-  </script>
